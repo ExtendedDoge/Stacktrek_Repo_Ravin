6e59bcab5a30d22b036f0b5e20647ac5d724896a
ExtendedDoge <opana.ravin25@gmail.com>
2022-10-05 20:17:37 +0800

    with pictures

--- sample-app-nav/src/component/TopBar.jsx
+++ sample-app-nav/src/component/TopBar.jsx
@@ -5,12 +5,11 @@ function TopBar() {
     return (
         <div className="topnav">
             <NavLink to="/home">Home</NavLink>
-            <NavLink to="/news">News</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink to="/movies">Movies</NavLink>
+            <NavLink to="/tourist">Tourist</NavLink>
             <NavLink to="/about">About</NavLink>
         </div>
     )
 }
-
 
 export default TopBar
